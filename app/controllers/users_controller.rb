2ce3a5de523c595026db4fb61a9f34283724a549
Yasmine <y.hartung@gmail.com>
2019-07-18 10:36:15 -0400

    working backend with fetches. need to continue customizing serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
         @users = User.all 
 
         render :json => @users
+    end
 
+    def user_clients
+        @user = User.all.find{|user| user.id == params[:id].to_i}
+        @clients = @user.clients
+
+        render :json => @clients
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-  resources :clients
+  get '/users', to: 'users#index'
+  get '/users/:id/clients', to:'users#user_clients' 
+  
 end
